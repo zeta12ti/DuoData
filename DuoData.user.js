2ce4ddce318c11bb5e73d847b6ac12176d5067e0
zeta12ti <zeta12ti@users.noreply.github.com>
2017-06-23 18:20:52 -0600

    Add xp to every course

--- DuoData.user.js
+++ DuoData.user.js
@@ -9,9 +9,12 @@
 // ==/UserScript==
 
 
+var inverse_flags = {"OgUIe": "hi", "_107sn": "id", "_12U6e": "ro", "_1ARRD": "ar", "_1KtzC": "vi", "_1PruQ": "it", "_1S3hi": "hu", "_1ct7y _2XSZu": "medium-circle-flag", "_1eqxJ": "ru", "_1fajz": "nl-NL", "_1h0xh": "da", "_1jO8h": "cy", "_1q_MQ": "tl", "_1tJJ2": "tr", "_1uPQW": "cs", "_1vhNM": "ga", "_1zZsN": "uk", "_200jU": "no-BO", "_24xu4": "gn", "_2DMfV": "sv", "_2KQN3": "fr", "_2N-Uj": "ja", "_2TXAL": "bn", "_2XSZu": "_circle-flag", "_2cR-E": "en", "_2gNgd": "zh-CN", "_2lkzc": "ko", "_2oTcA": "th", "_2tQo9": "el", "_3AA1F": "dk", "_3PU7E _2XSZu": "small-circle-flag", "_3T1km": "sw", "_3i5IF _2XSZu": "micro-circle-flag", "_3uusw": "pl", "_3viv6": "_flag", "_6mRM": "tlh", "_PDrK": "he", "mc4rg": "ca", "oboa9": "de", "pWj0w": "eo", "pmGwL": "pt", "q_PD-": "sn", "t-XH-": "un", "u5W-o": "es", "xi6jQ": "zh"}
+function getLanguageFromFlag(flagCode) {
+    return inverse_flags[flagCode]
+}
 
-// Fun fact: all this was painfully extracted from Duolingo's source code.
-// This is essentially the exact code used for drawing the graph of xp gains.
+
 function getEpochSeconds() {
     return Math.floor(Date.now() / 1000)
 }
@@ -81,32 +84,6 @@ function getCurrentDailyXp() {
     return getXpToday(timezoneOffset, xpGains)
 }
 
-// Adds total xp to the dropdown menu
-function insertTotalXp(xp) {
-    if (document.querySelectorAll('._1oVFS').length === 0) {
-        return
-    }
-    if (document.getElementById('total-xp') !== null) {
-        return updateTotalXp(xp)
-    }
-
-    var xpElement = document.createElement('span')
-
-    var text = document.createTextNode(' ' + xp + ' xp')
-    xpElement.appendChild(text)
-    xpElement.id = 'total-xp'
-    xpElement.classList.add('_1fA14')
-
-    document.querySelector('._1oVFS').appendChild(xpElement)
-}
-
-
-// Updates an existing total xp indicator
-function updateTotalXp(xp) {
-    var xpElement = document.getElementById('total-xp')
-    xpElement.firstChild.textContent = ' ' + xp + ' xp'
-}
-
 
 // Adds daily xp to the streak indicator
 function insertDailyXp(xp) {
@@ -133,10 +110,66 @@ function updateDailyXp(xp) {
 }
 
 
+// Adds data to the course list to indicate languages
+function addCourseData() {
+    if (document.querySelectorAll('._2kNgI').length === 0) {return false}
+
+    var courses = document.querySelectorAll('._2kNgI')
+    var fromLanguage = duo.uiLanguage
+    len = courses.length
+    for (var i=0, bigFlag, littleFlag; i<len; i++) {
+        if (courses[i].querySelectorAll('._2IJLr').length === 0) {
+            bigFlag = courses[i].querySelector('._3vx2Z')
+            courses[i].setAttribute('data-learning', getLanguageFromFlag(bigFlag.classList[0]))
+            courses[i].setAttribute('data-from', fromLanguage)
+        }
+        else {
+            bigFlag = courses[i].querySelector('._2IJLr')
+            littleFlag = courses[i].querySelector('._2c_Ro')
+            courses[i].setAttribute('data-learning', getLanguageFromFlag(bigFlag.classList[0]))
+            courses[i].setAttribute('data-from', getLanguageFromFlag(littleFlag.classList[0]))
+        }
+    }
+    return true
+}
+
+
+// Adds xp indicators for every course
+function insertCourseXp() {
+    if (addCourseData()) {
+        var courses = document.querySelectorAll('._2kNgI')
+        var duoStateCourses = JSON.parse(localStorage['duo.state']).courses
+        len = courses.length
+        for (var i=0, learningLanguage, fromLanguage, xp, xpElement, text; i<len; i++) {
+            learningLanguage = courses[i].getAttribute('data-learning')
+            fromLanguage = courses[i].getAttribute('data-from')
+            xp = duoStateCourses[learningLanguage + '<' + fromLanguage].xp
+            if (document.getElementById(learningLanguage + '-' + fromLanguage + '-xp') === null) {
+                xpElement = document.createElement('span')
+
+                text = document.createTextNode(' ' + xp + ' xp')
+                xpElement.appendChild(text)
+                xpElement.id = learningLanguage + '-' + fromLanguage + '-xp'
+                xpElement.classList.add('_1fA14')
+
+                courses[i].appendChild(xpElement)
+            }
+            else {
+                xpElement = document.getElementById(learningLanguage + '-' + fromLanguage + '-xp')
+                xpElement.firstChild.textContent = ' ' + xp + ' xp'
+            }
+        }
+    }
+}
+            
+    
+            
+
+
+// Adds/updates everything
 async function routine() {
-    var totalXp = getCurrentTotalXp()
+    insertCourseXp()
     var dailyXp = getCurrentDailyXp()
-    insertTotalXp(totalXp)
     insertDailyXp(dailyXp)
 }
 
